6d5d57d9a149168f1f98976656186ec2f2401912
faizaladin <faizaladin365@gmail.com>
2023-05-07 23:00:24 -0400

    cleaned up version

--- src/instabot.py
+++ src/instabot.py
@@ -1,3 +1,4 @@
+from random import randint
 import sys
 import time
 import yaml
@@ -44,32 +45,15 @@ class Instabot:
         time.sleep(5)
         self.driver.find_element("xpath", "//a[contains(@href,'/followers')]").click()
         time.sleep(5)
-        # wait for page to load
-        # Get all the followers
-        #self.driver.find_element("xpath", "/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/button").click()
-        follower = self.driver.find_element("xpath", "/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]")
-        removebutton0 = follower.find_element("xpath", "/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div/div/div[3]/div/div")
-        removebutton0.click()
-        time.sleep(5)
-        removebutton1 = self.driver.find_element("xpath", "/html/body/div[2]/div/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]")
-        removebutton1.click()
-        #button = follower.driver.find_element(By.CSS_SELECTOR, "//div//button[div[text()='Remove']")
-        #follower.click()
-        #print(follower)
-        
-        #print(followers)
-        # Remove the followers that are in the keep_followers_list
-        # if keep_followers_list:
-        #     followers = [f for f in followers if f.text.split("\n")[0] not in keep_followers_list]
-        # # Unfollow the remaining followers
-        # for follower in followers:
-        #     # Get the follow button of the follower
-        #     button = follower.find_element("xpath", "//button[text()='Remove']")
-        #     button.click()
-        #     # Click the "Unfollow" button on the unfollow confirmation popup
-        #     self.driver.find_element("xpath","//button[text()='Remove']").click()
-        #     # Wait for 2 seconds before unfollowing the next user
-        #     time.sleep(2)
+        for i in range(1,5):
+            follower = self.driver.find_element("xpath", f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[{i}]")
+            #follower_name = self.driver.find_element("xpath", f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div/div/div[2]/div/div/span[1]/span/div/div/div/a/span/div")
+            removebutton0 = follower.find_element("xpath", f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[{i}]/div/div/div/div[3]/div/div")
+            removebutton0.click()
+            time.sleep(5)
+            removebutton1 = self.driver.find_element("xpath", "/html/body/div[2]/div/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]")
+            removebutton1.click()
+            time.sleep(randint(3, 6))
         print("done")
 
     def close(self):
@@ -80,3 +64,9 @@ if __name__ == '__main__':
     keep_followers_list = ['francisco.moreira15', 'haley.lowenthal', '84nrogers', 'ellarudisill', 'khe_levy', 'chesniecheung', 'j1njee', 'nickyph34', 'billtrn_', 'williemears', 'mortn4sty', 'jason.hwong', 'sam.rubin99', 'priyaabakshi', 'genna.bishop', 'freddie_lemons', 'mala.krish', 'rayan.tzd', 'pono_demarzo', 'averyychen', '_xuanyi.wang', 'fernseph', 'sana.aladin']
     instabot.remove_followers(keep_followers_list)
     instabot.close()
+
+# first user /html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]
+
+#second user /html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[2]
+
+#first user remove button /html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div/div/div/div[2]/div/div/span[1]/span/div/div/div/a/span/div
